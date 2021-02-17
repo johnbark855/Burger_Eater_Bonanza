4f16b8db2a04c2bda0dc44b1f7ab806b6e35657c
Alex Gold <johnbark855@gmail.com>
2021-02-17 03:58:00 -0500

    new file added

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+const mysql = require("mysql");
+
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    ç
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
+};
+
+connection.connect((err) => {
+    if (err) {
+        console.log(`error connecting: ${err.stack}`);
+        return;
+    }
+    console.log(`connected as id ${connection.threadId}`);
+});
+
+module.exports = connection;
