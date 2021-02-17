a3943428a94fd1bfd5fdaf697cdcd7f1b9025009
john bark <johnbark855@gmail.com>
2021-02-17 02:17:16 -0500

    updated and added files

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Classic", 1);
+
+INSERT INTO burgers (burger_name)
+VALUES ("Bacon");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Double");
