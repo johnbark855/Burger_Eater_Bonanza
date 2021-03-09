12dc16458385aea3a6649fc6464ea84ce04feeb4
john bark <johnbark855@gmail.com>
2021-03-09 15:09:49 -0500

    fixed errors again

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 const express = require("express");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers/burgersController.js")
@@ -23,30 +21,4 @@ app.use(routes);
 
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
-=======
-
-const express = require("express");
-const exphbs = require("express-handlebars");
-const routes = require("./controllers/burgersController.js")
-
-const PORT = process.env.PORT || 8080;
-const app = express();
-
-
-app.use(express.static('public'));
-
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-
-app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-
-app.use(routes);
-
-app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
->>>>>>> 419a9a5b4e5f88a9bae40eb62805ea70e84d578a
 });
