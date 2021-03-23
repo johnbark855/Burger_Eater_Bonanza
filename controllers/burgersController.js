7bf7c01d5db0ffafdc872228b4cf9aaa2828c45c
john bark <johnbark855@gmail.com>
2021-03-23 19:21:34 -0400

    final fixes

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,34 +1,46 @@
-const express = require("express");
+
+const express = require('express');
+
 const router = express.Router();
 
-const burger = require("../models/burger.js");
 
-router.get("/", (req, res) => {
+const burger = require('../models/burger.js');
+
+router.get('/', (req, res) => {
     burger.allburger((data) => {
         const hbsObject = {
             burgers: data,
         };
-        console.log("hbsObject", hbsObject);
+        console.log('hbsObject', hbsObject);
         res.render('index', hbsObject);
     });
 });
 
 router.post('/api/burgers', (req, res) => {
-    const condition = 'id = $req.params.id}';
-    console.log("condition", condition)
-
-    burger.updateburger(
-        {
-            devoured: req.body.devoured,
-        },
-        condition,
-        (result) => {
-            if (result.changedRows === 0) {
-
-                return res.status(404).end();
-            }
-            res.status(200).end();
-        }
-    );
+    burger.createburger(['burger_name', 'devoured'], [req.body.burger_name, 0], (result) => {
+        res.redirect('/');
+    });
 });
-module.exports = router;
+
+router.put('/api/burgers/:id', (req, res) => {
+    const condition = `id = ${req.params.id}`;
+  
+    console.log('condition', condition);
+  
+    burger.updateburger(
+      {
+        devoured: req.body.devoured,
+      },
+      condition,
+      (result) => {
+        if (result.changedRows === 0) {
+          
+          return res.status(404).end();
+        }
+        res.status(200).end();
+      }
+    );
+  });
+
+
+module.exports = router; 
